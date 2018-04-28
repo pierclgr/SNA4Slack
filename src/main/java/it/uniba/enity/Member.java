06c9732b201fc69cb6bc65432bb321d259b0b43e
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2018-04-28 17:53:17 +0200

    Implementa la classe fondamentale Member
    Implementata la classe fondamentale Member con i relativi attributi, costruttori e metodi per ottenere gli attributi

--- src/main/java/it/uniba/enity/Member.java
+++ src/main/java/it/uniba/enity/Member.java
@@ -1,5 +1,19 @@
 package it.uniba.enity;
 
 public class Member {
-
+	private String id;
+	private String realName;
+	
+	public Member(String id, String realName) {
+		this.id=id;
+		this.realName=realName;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getRealName() {
+		return realName;
+	}
 }
