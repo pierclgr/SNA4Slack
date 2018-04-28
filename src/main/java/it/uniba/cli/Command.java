91ad343053b8470bb2bfa9cda617a9ccba1cc2b2
Nunzia Andrulli <nunziaandrulli@gmail.com>
2018-04-28 19:59:41 +0200

    Implementata la classe Command
    Implementata la classe Command con i relativi attributi, costruttori e metodi per ottenere gli attributi

--- src/main/java/it/uniba/cli/Command.java
+++ src/main/java/it/uniba/cli/Command.java
@@ -1,5 +1,25 @@
 package it.uniba.cli;
 
 public class Command {
-
+	String name;
+	String options;
+	String description;
+	
+	Command(String name, String options, String description){
+		this.name = name;
+		this.options = options;
+		this.description = description;
+	}
+	
+	String getName() {
+		return this.name;
+	}
+	
+	String getOptions() {
+		return this.options;
+	}
+	
+	String getDescription() {
+		return this.description;
+	}
 }
