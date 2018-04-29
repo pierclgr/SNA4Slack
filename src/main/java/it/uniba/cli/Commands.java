511498552602db06e01d640b98736be3f2eda4f7
Rocco Debellis <rocdebellis@gmail.com>
2018-04-29 17:33:33 +0200

    Correggi classe Commands
    Modificata la classe Commands utilizzando una soluzione alternativa all'utilizzo dei file

--- src/main/java/it/uniba/cli/Commands.java
+++ src/main/java/it/uniba/cli/Commands.java
@@ -1,92 +1,34 @@
 package it.uniba.cli;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-
-import it.uniba.file.PathManager;
+import java.util.ListIterator;
 
 public class Commands {
 	private List<Command> commands;
-	private int numberOfAllCommands;
 
 	public Commands() throws IOException{
 		this.commands = new LinkedList<Command>();
-		numberOfAllCommands=calculateNumberOfCommands();
-		for (int j = 0; j < numberOfAllCommands; j++) {
-			Command command = new Command(readCommand(j), readOptions(j), readDescription(j));
-			addCommand(command);
-		}
+		commands.add(new Command("members","-f fileName","Get all members from \"fileName\" zip file"));
+		commands.add(new Command("channels","-f fileName","Get all channels from \"fileName\" zip file"));
+		commands.add(new Command("members","-ch channelName -f fileName","Get all members of \"channelName\" channel from \"fileName\" zip file"));
+		commands.add(new Command("members","-ch -f fileName","Get all members for all channels from \"fileName\" zip file"));
 	}
 	
-	int calculateNumberOfCommands() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(PathManager.getAbsolutePath("res/files/commands.txt")));
-		String line = reader.readLine();
-		int i = 0;
-		while (line != null) {
-			line = reader.readLine();
-			i++;
-		}
-		reader.close();
-		return i;
-	}
-	
-	public int getNumberOfCommands() {
-		return commands.size();
-	}
-
-	public Command getCommand(int i) {
-		return this.commands.get(i);
-	}
-
-	public void addCommand(Command c) {
-		this.commands.add(c);
-	}
-
-	public String readCommand(int i) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(PathManager.getAbsolutePath("res/files/commands.txt")));
-		String line = reader.readLine();
-		int j = 0;
-		while (line != null && j<i) {
-			line = reader.readLine();
-			j++;
-		}
-		reader.close();
-		return line;
+	public LinkedList<Command> getCommands(){
+		return (LinkedList<Command>) this.commands;
 	}
 
 	boolean contains(Command c) {
-		boolean found  = false;
-		for(int i=0; i<commands.size()&&!found; i++) {
-			if(this.getCommand(i).getName().equals(c.getName()))
-				found = true;
+		boolean found=false;
+		ListIterator<Command> iterator=(ListIterator<Command>) commands.iterator();
+		while(iterator.hasNext()&&!found) {
+			Command curr=iterator.next();
+			if(curr.getName().equals(c.getName())) {
+				found=true;
+			}
 		}
 		return found;
-	}
-
-	String readOptions(int i) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(PathManager.getAbsolutePath("res/files/options.txt")));
-		String line = reader.readLine();
-		int j=0;
-		while (line != null && j<i) {
-			line = reader.readLine();
-			j++;
-		}
-		reader.close();
-		return line;
-	}
-	
-	String readDescription(int i) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(PathManager.getAbsolutePath("res/files/descriptions.txt")));
-		String line = reader.readLine();
-		int j=0;
-		while (line != null && j<i) {
-			line = reader.readLine();
-			j++;
-		}
-		reader.close();
-		return line;
 	}
 }
