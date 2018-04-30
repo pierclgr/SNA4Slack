9a926a2e343ffca54b105a6e30180129a14d8655
Rocco Debellis <rocdebellis@gmail.com>
2018-04-30 10:42:00 +0200

    Aggiungi funzione per comando Members
    Aggiunta la funzione nella classe CommandManager per ottenere tutti i membri

--- src/main/java/it/uniba/cli/CommandManager.java
+++ src/main/java/it/uniba/cli/CommandManager.java
@@ -1,5 +1,37 @@
 package it.uniba.cli;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import it.uniba.entity.Member;
+import it.uniba.entity.Workspace;
+import it.uniba.file.PathManager;
+import it.uniba.file.zip.FileNotInZipException;
+import it.uniba.file.zip.NotValidWorkspaceException;
+
+
 public class CommandManager {
 
+	public static void getMembers(String workspace) {
+		try {
+			Workspace slackWorkspace=new Workspace(PathManager.getAbsolutePath(workspace));
+			LinkedList<Member> workspaceMembers=slackWorkspace.getAllMembers();
+			ListIterator<Member> membersIterator=(ListIterator<Member>) workspaceMembers.iterator();
+			while(membersIterator.hasNext()) {
+				System.out.println(membersIterator.next().getRealName());
+			}
+		} catch (IOException e) {
+			if(e instanceof FileNotFoundException||e instanceof NoSuchFileException) {
+				System.out.println(PathManager.getAbsolutePath(workspace)+" not found");
+			}else {
+				e.printStackTrace();
+			}
+		} catch (FileNotInZipException|NotValidWorkspaceException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
 }
