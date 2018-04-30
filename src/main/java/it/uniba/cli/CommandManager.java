ad1a5f276b61270c3156b3ee93872bfd61a04992
Nunzia Andrulli <nunziaandrulli@gmail.com>
2018-04-30 11:31:51 +0200

    Aggiungi funzione per comando Help
    Aggiunta la funzione nella classe CommandManager per ottenere informazioni di help

--- src/main/java/it/uniba/cli/CommandManager.java
+++ src/main/java/it/uniba/cli/CommandManager.java
@@ -1,5 +1,30 @@
 package it.uniba.cli;
 
-public class CommandManager {
+import java.util.ListIterator;
 
+public class CommandManager {
+	public static void help(){
+		Commands commands=new Commands();
+		int maxNumCharCommand=0;
+		int maxNumCharDescription=0;
+		ListIterator<Command> commandsIterator=(ListIterator<Command>) commands.getCommands().iterator();
+		while (commandsIterator.hasNext()) {
+			Command curr=commandsIterator.next();
+			if((curr.getName()+" "+curr.getOptions()).length()>=maxNumCharCommand) {
+				maxNumCharCommand=(curr.getName()+" "+curr.getOptions()).length();
+			}
+			if(curr.getDescription().length()>=maxNumCharDescription) {
+				maxNumCharDescription=curr.getDescription().length();
+			}
+		}
+		System.out.format("%"+maxNumCharCommand+"s\t%"+maxNumCharDescription+"s","COMMAND","DESCRIPTION");
+		System.out.println();
+		System.out.println();
+		commandsIterator=(ListIterator<Command>) commands.getCommands().iterator();
+		while (commandsIterator.hasNext()) {
+			Command curr=commandsIterator.next();
+			System.out.format("%"+maxNumCharCommand+"s\t%"+maxNumCharDescription+"s",curr.getName()+" "+curr.getOptions(),curr.getDescription());
+			System.out.println();
+		}
+	}
 }
