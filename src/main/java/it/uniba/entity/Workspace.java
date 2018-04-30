14307b7565a1bab3a5b63fb2966288af01ba3d32
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2018-04-30 18:51:41 +0200

    Modifica classe Workspace
    Modificata la classe Workspace aggiungendo la gestione della nuova eccezione

--- src/main/java/it/uniba/entity/Workspace.java
+++ src/main/java/it/uniba/entity/Workspace.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.zip.ZipException;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import it.uniba.file.zip.FileNotInZipException;
 import it.uniba.file.zip.NotValidWorkspaceException;
+import it.uniba.file.zip.NotZipFileException;
 import it.uniba.file.zip.Zip;
 
 public class Workspace {
@@ -16,33 +18,37 @@ public class Workspace {
 	List<Channel> channels;
 	List<Member> members;
 	
-	public Workspace(String workspaceZipFile) throws IOException,NotValidWorkspaceException,FileNotInZipException {
-		Zip workspaceZip=new Zip(workspaceZipFile);
-		if(!workspaceZip.contains("channels.json")||!workspaceZip.contains("users.json")) {
-			throw new NotValidWorkspaceException(workspaceZipFile);
-		}else {
-			this.workspaceZip=workspaceZip;
-			this.channels=new LinkedList<Channel>();
-			this.members=new LinkedList<Member>();
-
-			JSONArray membersRootArray = new JSONArray(workspaceZip.getFileContent("users.json"));
-			for(int i=0; i < membersRootArray.length(); i++) {
-				JSONObject member = membersRootArray.getJSONObject(i);
-				Member currMember=new Member(member.getString("id"),member.getString("real_name"));
-				members.add(currMember);
-			}
-			JSONArray channelsRootArray = new JSONArray(workspaceZip.getFileContent("channels.json"));
-			for(int j=0; j<channelsRootArray.length(); j++) {
-				JSONObject channel= channelsRootArray.getJSONObject(j);
-				Channel currChannel=new Channel(channel.getString("id"),channel.getString("name"));
-				JSONArray membersArray = channel.getJSONArray("members");
-				for(int k=0; k<membersArray.length(); k++){
-					String memberId=membersArray.getString(k);
-					currChannel.getMembers().add(getMemberById(memberId));
-					getMemberById(memberId).getChannels().add(currChannel);
+	public Workspace(String workspaceZipFile) throws IOException,NotValidWorkspaceException,FileNotInZipException,NotZipFileException {
+		try {
+			Zip workspaceZip=new Zip(workspaceZipFile);
+			if(!workspaceZip.contains("channels.json")||!workspaceZip.contains("users.json")) {
+				throw new NotValidWorkspaceException(workspaceZipFile);
+			}else {
+				this.workspaceZip=workspaceZip;
+				this.channels=new LinkedList<Channel>();
+				this.members=new LinkedList<Member>();
+	
+				JSONArray membersRootArray = new JSONArray(workspaceZip.getFileContent("users.json"));
+				for(int i=0; i < membersRootArray.length(); i++) {
+					JSONObject member = membersRootArray.getJSONObject(i);
+					Member currMember=new Member(member.getString("id"),member.getString("real_name"));
+					members.add(currMember);
 				}
-				channels.add(currChannel);
+				JSONArray channelsRootArray = new JSONArray(workspaceZip.getFileContent("channels.json"));
+				for(int j=0; j<channelsRootArray.length(); j++) {
+					JSONObject channel= channelsRootArray.getJSONObject(j);
+					Channel currChannel=new Channel(channel.getString("id"),channel.getString("name"));
+					JSONArray membersArray = channel.getJSONArray("members");
+					for(int k=0; k<membersArray.length(); k++){
+						String memberId=membersArray.getString(k);
+						currChannel.getMembers().add(getMemberById(memberId));
+						getMemberById(memberId).getChannels().add(currChannel);
+					}
+					channels.add(currChannel);
+				}
 			}
+		}catch(ZipException e) {
+			throw new NotZipFileException(workspaceZipFile);
 		}
 	}
 	
