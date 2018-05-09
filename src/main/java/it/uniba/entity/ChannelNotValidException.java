8f7a2abd7c5030a77b5e1de32ada70d486a68d80
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2018-05-09 21:55:00 +0200

    Aggiungi classe canale non valido
    Aggiunta classe eccezione che gestisce il caso in cui l'utente inserisca un canale non valido

---
+++ src/main/java/it/uniba/entity/ChannelNotValidException.java
@@ -0,0 +1,19 @@
+package it.uniba.entity;
+
+public class ChannelNotValidException extends Throwable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1530588041264557265L;
+	private String message=" is not a valid channel";
+	
+	public ChannelNotValidException(String channel) {
+		message=channel+message;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+	
+}	
