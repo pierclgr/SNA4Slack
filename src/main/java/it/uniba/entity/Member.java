2eefb62d7da1bbaeef6c686436e90b243524759d
Rocco Debellis <rocdebellis@gmail.com>
2018-05-19 19:08:51 +0200

    Modifica classe Member
    Aggiunto supporto per l'id ai metodi

--- src/main/java/it/uniba/entity/Member.java
+++ src/main/java/it/uniba/entity/Member.java
@@ -9,7 +9,7 @@ public class Member {
 	private String real_name;
 	private String display_name;
 	private List<Channel> channels;
-	
+
 	public Member(String id, String name, String real_name, String display_name) {
 		this.id=id;
 		this.name=name;
@@ -17,21 +17,23 @@ public class Member {
 		this.display_name=display_name;
 		this.channels=new LinkedList<Channel>();
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getName() {
 		if(!display_name.equals("")) {
 			return display_name;
 		}else if(!real_name.equals("")) {
 			return real_name;
-		}else {
+		}else  if(!name.equals("")) {
 			return name;
+		}else {
+			return id;
 		}
 	}
-	
+
 	public boolean isUser(String member) {
 		if(display_name.equals(member)) {
 			return true;
@@ -39,15 +41,17 @@ public class Member {
 			return true;
 		}else if(name.equals(member)) {
 			return true;
+		}else if(id.equals(member)) {
+			return true;
 		}else {
 			return false;
 		}
 	}
-	
+
 	public String getUserName() {
 		return name;
 	}
-	
+
 	public LinkedList<Channel> getChannels(){
 		return (LinkedList<Channel>) channels;
 	}
